7a515e56ead93df4d7b1bbf4feeecb74f9285aa4
Professor Neto <professorneto2023@gmail.com>
2025-02-10 20:26:04 -0300

    Aula 10/02

--- index.php
+++ index.php
@@ -8,6 +8,47 @@
     <link rel="stylesheet" href="css/styles.css" />
 </head>
 <body>
+
+<div class="interface">
+
+    <header class="cabecalho">
+
+        <nav class="menu">
+
+            <div class="logo">
+                <img src="imagens/logo.png" alt="LOGO" width="150" />
+            </div>
+
+            <ul>
+                <li>Home</li>
+                <li>Serviços</li>
+                <li>Produtos</li>
+                <li>Portifólio</li>
+                <li>Sobre</li>
+                <li>Contato</li>
+            </ul>
+
+        </nav>
+
+        <div class="titles">
+
+            <h1 class="titulo">Entregando serviços de qualidade</h1>
+            <h1 class="subtitulo">Soluções em TI</h1>
+
+            <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam ab in! A, nam voluptas!</p>
+
+        </div>
+
+        <div class="socialIcons">
+            <a href="#">Twitter</a>
+            <a href="#">Facebook</a>
+            <a href="#">LinkedIn</a>
+            <a href="#">Instagram</a>
+        </div>
+
+    </header>
+
+</div>
     
 </body>
 </html>
